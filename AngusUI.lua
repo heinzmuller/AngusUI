e1249807e006da26c79941de16d2ef05bb94f12e
Alexander Nilsson <alexander.nilsson@epinova.no>
2024-08-01 12:45:06 +0200

    Update to 11.0.0

--- AngusUI.lua
+++ AngusUI.lua
@@ -1,137 +1,87 @@
 local _, angusui = ...
 
-local darkness = .5
+local darkness = .4
 
 local frame = CreateFrame("Frame")
 frame:RegisterEvent("ADDON_LOADED")
 frame:RegisterEvent("PLAYER_ENTERING_WORLD")
 
-function slashCommand(arg)
-	if (arg == "bar") then
-		resetActionBar()
-	else
-		print("These are the commands you're looking for")
-		print("/bar")
-	end
+function Set(list)
+    local set = {}
+    for _, l in ipairs(list) do
+        set[l] = true
+    end
+    return set
 end
 
-function resetActionBar()
-	-- Hide actionbar page stuff
-	-- local hide = { MainMenuBarArtFrame.PageNumber, ActionBarUpButton, ActionBarDownButton }
-	-- for i, v in pairs(hide) do
-	-- 	v:SetShown(false)
-	-- end
-
-	-- Show MultiBarBottomRight :)
-	local r = MultiBarBottomRight
-	r:SetShown(true)
-
-	-- Adjust anchors for button 7
-	local b = MultiBarBottomRightButton7
-	b:ClearAllPoints()
-	b:SetPoint("LEFT", MultiBarBottomRightButton6, "RIGHT", 6, 0)
+function SlashCommand(arg)
+    if (arg == "back") then
+        TeleportBack()
+    else
+        print("These are the commands you're looking for")
+        print("/back")
+    end
 end
 
-SlashCmdList.ANGUSUI = slashCommand
+function TeleportBack()
+    print("So you wanna teleport do you?")
+
+    local cloaks = Set { 65274 }
+
+    local itemId = GetInventoryItemID("player", 15)
+    print("itemId:" .. itemId)
+
+    function EquipTeleportCloak()
+        print("Equipping teleport cloak")
+        for i = 0, NUM_BAG_SLOTS do
+            for z = 1, C_Container.GetContainerNumSlots(i) do
+                if C_Container.GetContainerItemID(i, z) == 65274 then
+                    local _, duration = C_Container.GetContainerItemCooldown(i, z)
+                    if duration == 0 then
+                        print("Item is ready!")
+                        C_Item.EquipItemByName(65274)
+                    else
+                        print("Cooldown is " .. duration)
+                    end
+                    break
+                end
+            end
+        end
+    end
+
+    if cloaks[itemId] then
+        -- do something
+        print("You have cloak equipped" .. itemId)
+    else
+        print("You NOT have equipped" .. itemId)
+        EquipTeleportCloak()
+    end
+end
+
+SlashCmdList.ANGUSUI = SlashCommand
 SLASH_ANGUSUI1, SLASH_ANGUSUI2 = "/angusui", "/aui"
 function frame:ADDON_LOADED(self, addon)
-	if (addon == "Blizzard_TimeManager") then
-		for i, v in pairs(
-			{
-				PartyMemberFrame1Texture,
-				PartyMemberFrame2Texture,
-				PartyMemberFrame3Texture,
-				PartyMemberFrame4Texture,
-				PartyMemberFrame1PetFrameTexture,
-				PartyMemberFrame2PetFrameTexture,
-				PartyMemberFrame3PetFrameTexture,
-				PartyMemberFrame4PetFrameTexture,
-				FocusFrameToTTextureFrameTexture,
-				BonusActionBarFrameTexture0,
-				BonusActionBarFrameTexture1,
-				BonusActionBarFrameTexture2,
-				BonusActionBarFrameTexture3,
-				BonusActionBarFrameTexture4,
-				MainMenuBarTexture0,
-				MainMenuBarTexture1,
-				MainMenuBarTexture2,
-				MainMenuBarTexture3,
-				MainMenuMaxLevelBar0,
-				MainMenuMaxLevelBar1,
-				MainMenuMaxLevelBar2,
-				MainMenuMaxLevelBar3,
-				MinimapBorder,
-				CastingBarFrameBorder,
-				FocusFrameSpellBarBorder,
-				TargetFrameSpellBarBorder,
-				MiniMapTrackingButtonBorder,
-				MiniMapLFGFrameBorder,
-				MiniMapBattlefieldBorder,
-				MiniMapMailBorder,
-				MinimapBorderTop,
-				MainMenuBarArtFrameBackground.BackgroundLarge,
-				MainMenuBarArtFrameBackground.BackgroundSmall,
-				StatusTrackingBarManager.SingleBarLarge,
-				StatusTrackingBarManager.SingleBarSmall,
-				PlayerFrameTexture,
-				TargetFrameTextureFrameTexture,
-				select(1, TimeManagerClockButton:GetRegions())
-			}
-		) do
-			v:SetVertexColor(darkness, darkness, darkness)
-		end
+    if (addon == "Blizzard_TimeManager") then
+        for i, v in pairs(
+            {
+                MainMenuBar.BorderArt,
+                MainMenuBar.EndCaps.LeftEndCap,
+                MainMenuBar.EndCaps.RightEndCap,
+                MinimapCompassTexture
+            }
+        ) do
+            v:SetVertexColor(darkness, darkness, darkness)
+        end
 
-		for i, v in pairs({select(2, TimeManagerClockButton:GetRegions())}) do
-			v:SetVertexColor(1, 1, 1)
-		end
-
-		MainMenuBarArtFrame.LeftEndCap:SetVertexColor(darkness, darkness, darkness)
-		MainMenuBarArtFrame.RightEndCap:SetVertexColor(darkness, darkness, darkness)
-
-		-- TODO: Mouseover alpha
-		MicroButtonAndBagsBar:SetAlpha(0.2)
-
-		UIParent:SetScale(.55)
-
-		self:UnregisterEvent("ADDON_LOADED")
-	end
+        self:UnregisterEvent("ADDON_LOADED")
+    end
 end
 
 frame:SetScript(
-	"OnEvent",
-	function(self, event, ...)
-		hooksecurefunc(
-			"InterfaceOptions_UpdateMultiActionBars",
-			function()
-				SHOW_MULTI_ACTIONBAR_2 = false
-
-				UIPARENT_MANAGED_FRAME_POSITIONS["MultiBarBottomRight"] = {
-					baseY = 2,
-					watchBar = 1,
-					maxLevel = 1,
-					anchorTo = "MultiBarBottomLeft",
-					point = "TOPLEFT",
-					rpoint = "TOPLEFT",
-					xOffset = 0,
-					yOffset = 55
-				}
-			end
-		)
-
-		hooksecurefunc(
-			MainMenuBar,
-			"ChangeMenuBarSizeAndPosition",
-			function(var)
-				resetActionBar()
-			end
-		)
-
-		if (event == "ADDON_LOADED") then
-			self:ADDON_LOADED(self, ...)
-		end
-
-		if (event == "PLAYER_ENTERING_WORLD") then
-			MainMenuBar:SetScale(.8)
-		end
-	end
+    "OnEvent",
+    function(self, event, ...)
+        if (event == "ADDON_LOADED") then
+            self:ADDON_LOADED(self, ...)
+        end
+    end
 )
